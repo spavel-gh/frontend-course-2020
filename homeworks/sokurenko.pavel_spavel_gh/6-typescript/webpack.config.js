ece3a903148f639b64caf566051594c97a8ed697
spavel-gh <spavel.mail@gmail.com>
2020-11-28 17:21:01 +0200

    webpack: fixed and working.

--- homeworks/sokurenko.pavel_spavel_gh/6-typescript/webpack.config.js
+++ homeworks/sokurenko.pavel_spavel_gh/6-typescript/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.ts",
@@ -11,23 +9,12 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new CleanWebpackPlugin(),
-//     new HtmlWebpackPlugin({
-//       title: 'My title',
-//       template: 'src/index.html',
-//     }),
-//   ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
